4daa968b4ba9d207f23ea73eeb90bb6c1b83fc35
Jennifer Doblan <Jennifer.doblan@metalab.csun.edu>
2018-01-25 19:44:30 -0800

    first commit

---
+++ src/main/java/com/teamtreehouse/AppConfig.java
@@ -0,0 +1,8 @@
+package com.teamtreehouse;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class AppConfig {
+   ls
+}
